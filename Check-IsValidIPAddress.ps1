327c74048f3bbb6d5409b9d41cdfe985a6828cfb
My Random Thoughts <support@myrandomthoughts.co.uk>
2016-10-30 19:54:50 +0000

    Check-IsValidIPAddress

---
+++ Check-IsValidIPAddress.ps1
@@ -0,0 +1,8 @@
+﻿Function Check-IsValidIPAddress([string]$IPAddress)
+{
+    [boolean]$Octets = (($IPAddress.Split(".") | Measure-Object).Count -eq 4)
+    [boolean]$Valid  =  ($IPAddress -as [ipaddress]) -as [boolean]
+    Return  ($Valid -and $Octets)
+}
+
+Check-IsValidIPAddress -IPAddress '1.2.3.4'
