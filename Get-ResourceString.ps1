598852b1a0dc30c4c61eff884c6b0166f45b7076
My Random Thoughts <support@myrandomthoughts.co.uk>
2019-01-02 14:45:03 +0000

    Gets resource strings from within DLLs

--- Get-ResourceString.ps1
+++ Get-ResourceString.ps1
@@ -2,28 +2,23 @@
     Param([string]$path)
 
     [string]$code = @'
-        [DllImport("user32.dll", EntryPoint="LoadStringW", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
-        public static extern int LoadString(IntPtr hModule, int resourceID, StringBuilder resourceValue, int len);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "LoadLibraryExW")]
-        public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        public static extern int FreeLibrary(IntPtr hModule);
+        [DllImport("user32.dll", EntryPoint="LoadStringW", CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)] public static extern int LoadString(IntPtr hModule, int resourceID, StringBuilder resourceValue, int len);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, EntryPoint = "LoadLibraryExW")] public static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hFile, uint dwFlags);
+        [DllImport("kernel32.dll", ExactSpelling = true)] public static extern int FreeLibrary(IntPtr hModule);
 '@
 
+    $helper = Add-Type -MemberDefinition $code -Name ResourceStrings -Namespace Win32API -PassThru -UsingNamespace System.Text
     $info   = $path -split ','
-    $helper = Add-Type -MemberDefinition $code -Name 'ResourceStrings' -Namespace 'Win32API' -PassThru -UsingNamespace 'System.Text' -ErrorAction Ignore
     $file   = [System.Environment]::ExpandEnvironmentVariables( ($info[0] -replace '@','') )
     $id     = [Math]::Abs($info[1])
  
     [IntPtr]$hMod = $helper::LoadLibraryEx($file, [IntPtr]::Zero, 3)
-    If ($hMod -ne [IntPtr]::Zero) {
+    If($hMod -ne [IntPtr]::Zero)
+    {
         $sb = New-Object System.Text.StringBuilder(1024)
-        If ($helper::LoadString($hMod, $id, $sb, $sb.Capacity) -ne 0) { $sb.ToString() }
+        If($helper::LoadString($hMod, $id, $sb, $sb.Capacity) -ne 0) { $sb.ToString() }
         [void]$helper::FreeLibrary($hMod);
     }
-    
 }
 
 Get-ResourceString '@%windir%\diagnostics\system\Networking\DiagPackage.dll,-20002'
