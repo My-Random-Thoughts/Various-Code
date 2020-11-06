bbeb4bd8f641291c1ec7d378d9220b1b35398d3e
My Random Thoughts <support@myrandomthoughts.co.uk>
2020-11-06 15:04:42 +0000

    Initial Commit
    In response to the following issue: https://github.com/webmd-health-services/Carbon/issues/24

---
+++ Get-HostsFile.ps1
@@ -0,0 +1,51 @@
+﻿Function Get-HostsFile {
+<#
+    .SYNOPSIS
+        Split out the windows HOST file and converts it into a custom object
+
+    .DESCRIPTION
+        Split out the windows HOST file and converts it into a custom object
+
+    .PARAMETER InputFile
+        Use an alternative file instead of the default
+
+    .EXAMPLE
+        Get-HostsFile
+
+    .NOTES
+        For additional information please see my GitHub wiki page
+
+    .LINK
+        https://github.com/My-Random-Thoughts
+#>
+
+    Param (
+        $InputFile = "$env:SystemRoot\System32\drivers\etc\hosts"
+    )
+
+    ForEach ($line In (Get-Content -Path $InputFile)) {
+        If ((-not $line.Trim()) -or ($line.StartsWith('#'))) { Continue }
+        [string[]]$entry = ($line.Split(' ').Trim())
+
+        $result = [pscustomobject]@{
+            IpAddress = $entry[0].Trim()        
+            Hostname  = @()
+            Comment   = ''
+        }
+
+        # Do not start from 0, this should always be the IP address
+        For ($i = 1; $i -lt $entry.Count; $i++) {
+            If (-not $entry[$i].Trim()) { Continue }
+
+            If ($entry[$i].Trim() -ne '#') {
+                $result.Hostname += $entry[$i].Trim()
+            }
+            Else {
+                $result.Comment = ($entry[$($i + 1)..$($entry.Count - 1)] -join ' ').Trim()
+                Break
+            }
+        }
+
+        Write-Output $result
+    }
+}
